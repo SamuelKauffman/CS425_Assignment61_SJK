000680d473ea88b5151d1a927b75a404848c09e5
SamuelKauffman <112411760+SamuelKauffman@users.noreply.github.com>
2025-04-15 09:44:11 -0400

    Update helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -1,21 +1,40 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
 #define NUM_THREADS 5
 
 void* print_hello(void* thread_id) {
     long tid = (long)thread_id;
     printf("Hello World from thread ID: %ld\n", tid);
-    return NULL;
+    pthread_exit(NULL);
+}
+
+double get_time_diff(struct timeval start, struct timeval end) {
+    return (end.tv_sec - start.tv_sec) * 1e6 + (end.tv_usec - start.tv_usec);
 }
 
 int main() {
     pthread_t threads[NUM_THREADS];
-    for (long t = 0; t < NUM_THREADS; t++) {
-        pthread_create(&threads[t], NULL, print_hello, (void*)t);
+    struct timeval start, end;
+
+    gettimeofday(&start, NULL);
+    for (long i = 0; i < NUM_THREADS; i++) {
+        printf("Hello World from thread ID: %ld\n", i);
     }
-    for (int t = 0; t < NUM_THREADS; t++) {
-        pthread_join(threads[t], NULL);
+    gettimeofday(&end, NULL);
+    printf("Serial Time: %.0f microseconds\n", get_time_diff(start, end));
+
+    gettimeofday(&start, NULL);
+    for (long i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, print_hello, (void*)i);
     }
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    gettimeofday(&end, NULL);
+    printf("Parallel Time: %.0f microseconds\n", get_time_diff(start, end));
+
     return 0;
 }
