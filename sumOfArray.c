115d718cd24a9290851439f38fc88e59394b7baf
SamuelKauffman <112411760+SamuelKauffman@users.noreply.github.com>
2025-04-15 09:45:15 -0400

    Update sumOfArray.c

--- sumOfArray.c
+++ sumOfArray.c
@@ -1,20 +1,26 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #define NUM_THREADS 4
-#define ARRAY_SIZE 16
+#define ARRAY_SIZE 1000000
 
 int array[ARRAY_SIZE];
 int total_sum = 0;
 pthread_mutex_t sum_mutex;
 
+double get_time_diff(struct timeval start, struct timeval end) {
+    return (end.tv_sec - start.tv_sec) * 1e6 + (end.tv_usec - start.tv_usec);
+}
+
 void* partial_sum(void* arg) {
     long tid = (long)arg;
-    int local_sum = 0;
-    int start = tid * (ARRAY_SIZE / NUM_THREADS);
-    int end = start + (ARRAY_SIZE / NUM_THREADS);
+    int chunk = ARRAY_SIZE / NUM_THREADS;
+    int start = tid * chunk;
+    int end = (tid == NUM_THREADS - 1) ? ARRAY_SIZE : start + chunk;
 
+    int local_sum = 0;
     for (int i = start; i < end; i++) {
         local_sum += array[i];
     }
@@ -28,23 +34,35 @@ void* partial_sum(void* arg) {
 
 int main() {
     pthread_t threads[NUM_THREADS];
+    struct timeval start, end;
 
+    for (int i = 0; i < ARRAY_SIZE; i++) array[i] = 1;
+
+    int serial_sum = 0;
+    gettimeofday(&start, NULL);
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        array[i] = i + 1;
+        serial_sum += array[i];
     }
+    gettimeofday(&end, NULL);
+    printf("Serial Sum: %d\n", serial_sum);
+    printf("Serial Time: %.0f microseconds\n", get_time_diff(start, end));
 
+    total_sum = 0;
     pthread_mutex_init(&sum_mutex, NULL);
-
-    for (long t = 0; t < NUM_THREADS; t++) {
-        pthread_create(&threads[t], NULL, partial_sum, (void*)t);
+    gettimeofday(&start, NULL);
+    for (long i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, partial_sum, (void*)i);
     }
-
-    for (int t = 0; t < NUM_THREADS; t++) {
-        pthread_join(threads[t], NULL);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
     }
-
+    gettimeofday(&end, NULL);
     pthread_mutex_destroy(&sum_mutex);
 
-    printf("Total sum of array elements: %d\n", total_sum);
+    printf("Parallel Sum: %d\n", total_sum);
+    printf("Parallel Time: %.0f microseconds\n", get_time_diff(start, end));
+
     return 0;
+}
+
 }
